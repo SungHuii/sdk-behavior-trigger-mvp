ecb14b8b71221f87bca2d70648ae68002bb6b0d2
홍성휘 <gkemg2017@gmail.com>
2025-04-24 19:45:45 +0900

    docs: update ProjectController swagger api
    ProjectController Swagger Api 수정

--- src/main/java/com/behavior/sdk/trigger/project/controller/ProjectController.java
+++ src/main/java/com/behavior/sdk/trigger/project/controller/ProjectController.java
@@ -28,25 +28,48 @@ public class ProjectController {
 
    private final ProjectService projectService;
 
+   @ApiResponses({
+           @ApiResponse(responseCode = "201", description = "프로젝트 생성 성공",
+                   content = @Content(schema = @Schema(implementation = ProjectResponse.class))),
+           @ApiResponse(responseCode = "400", description = "잘못된 요청")
+   })
    @PostMapping
+   @Operation(summary = "프로젝트 생성", description = "새로운 프로젝트를 생성하고 SDK 키를 발급합니다.")
    public ResponseEntity<ProjectResponse> createProject (
            @RequestBody @Valid ProjectCreateRequest request) {
       ProjectResponse createdProject = projectService.createProject(request);
       return ResponseEntity.status(HttpStatus.CREATED).body(createdProject);
    }
 
+   @Operation(summary = "프로젝트 조회", description = "프로젝트 ID로 특정 프로젝트 정보를 조회합니다.")
+   @ApiResponses({
+              @ApiResponse(responseCode = "200", description = "프로젝트 조회 성공",
+                     content = @Content(schema = @Schema(implementation = ProjectResponse.class))),
+              @ApiResponse(responseCode = "404", description = "프로젝트를 찾을 수 없음")
+   })
    @GetMapping("/{projectId}")
    public ResponseEntity<ProjectResponse> getProject(@PathVariable UUID projectId) {
       ProjectResponse projectResponse = projectService.getProject(projectId);
       return ResponseEntity.status(HttpStatus.OK).body(projectResponse);
    }
 
+   @Operation(summary = "모든 프로젝트 조회", description = "모든 프로젝트 정보를 조회합니다.")
+    @ApiResponses({
+              @ApiResponse(responseCode = "200", description = "모든 프로젝트 조회 성공",
+                     content = @Content(schema = @Schema(implementation = ProjectResponse.class))),
+              @ApiResponse(responseCode = "404", description = "프로젝트를 찾을 수 없음")
+    })
    @GetMapping("/allProjects")
    public ResponseEntity<List<ProjectResponse>> getAllProjects() {
       List<ProjectResponse> projects = projectService.getAllProjects();
       return ResponseEntity.status(HttpStatus.OK).body(projects);
    }
 
+   @Operation(summary = "프로젝트 삭제", description = "프로젝트 ID로 특정 프로젝트를 soft-delete 처리해서 삭제합니다.")
+   @ApiResponses({
+           @ApiResponse(responseCode = "204", description = "프로젝트 삭제 성공"),
+           @ApiResponse(responseCode = "404", description = "프로젝트를 찾을 수 없음")
+   })
    @DeleteMapping("/{projectId}")
    public ResponseEntity<Void> deleteProject(@PathVariable UUID projectId) {
       projectService.deleteProject(projectId);
