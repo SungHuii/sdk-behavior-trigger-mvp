607035cefd51fba05b81a0c4d7476595f9cd70e3
홍성휘 <gkemg2017@gmail.com>
2025-05-16 15:38:44 +0900

    feat: impl Email domain
    Email 도메인 생성

---
+++ src/main/java/com/behavior/sdk/trigger/email_log/repository/EmailLogRepository.java
@@ -0,0 +1,12 @@
+package com.behavior.sdk.trigger.email_log.repository;
+
+import com.behavior.sdk.trigger.email_log.entity.EmailLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EmailLogRepository extends JpaRepository<EmailLog, UUID> {
+
+    List<EmailLog> findAllByVisitorAndDeletedAtIsNull(UUID visitorId);
+}
