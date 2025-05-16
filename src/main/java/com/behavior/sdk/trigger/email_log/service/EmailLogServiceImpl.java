bcb79d21fe56535e0fbc270568129cf91479127d
홍성휘 <gkemg2017@gmail.com>
2025-05-16 20:57:00 +0900

    feat: impl EmailLogServiceImpl
    EmailLogService 구현체 서비스 개발

---
+++ src/main/java/com/behavior/sdk/trigger/email_log/service/EmailLogServiceImpl.java
@@ -0,0 +1,46 @@
+package com.behavior.sdk.trigger.email_log.service;
+
+import com.behavior.sdk.trigger.email.enums.EmailStatus;
+import com.behavior.sdk.trigger.email_log.entity.EmailLog;
+import com.behavior.sdk.trigger.email_log.repository.EmailLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class EmailLogServiceImpl implements EmailLogService{
+
+    private final EmailLogRepository emailLogRepository;
+
+    @Override
+    public EmailLog createEmailLog(UUID visitorId, UUID templateId, EmailStatus status) {
+        EmailLog emailLog = EmailLog.builder()
+                .visitorId(visitorId)
+                .templateId(templateId)
+                .status(status)
+                .createdAt(LocalDateTime.now())
+                .build();
+        return emailLogRepository.save(emailLog);
+    }
+
+    @Override
+    public List<EmailLog> listLogsByVisitorId(UUID visitorId) {
+        return emailLogRepository.findAllByVisitorIdAndDeletedAtIsNull(visitorId);
+    }
+
+    @Override
+    public void softDeleteEmailLog(UUID emailLogId) {
+        EmailLog emailLog = emailLogRepository.findById(emailLogId)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 이메일 로그 ID"));
+        emailLog.setDeletedAt(LocalDateTime.now());
+        emailLogRepository.save(emailLog);
+    }
+
+
+}
