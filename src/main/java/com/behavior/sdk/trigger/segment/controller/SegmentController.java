7896cd402360125d66eb65f2f688a7b7dfdda46e
홍성휘 <gkemg2017@gmail.com>
2025-05-21 15:32:03 +0900

    feat: impl Segment api POST / GET / DELETE
    Segment 관리 API 구현
    POST / GET(단일, list) / DELETE(soft delete)

---
+++ src/main/java/com/behavior/sdk/trigger/segment/controller/SegmentController.java
@@ -0,0 +1,72 @@
+package com.behavior.sdk.trigger.segment.controller;
+
+import com.behavior.sdk.trigger.segment.dto.SegmentCreateRequest;
+import com.behavior.sdk.trigger.segment.dto.SegmentResponse;
+import com.behavior.sdk.trigger.segment.entity.Segment;
+import com.behavior.sdk.trigger.segment.entity.SegmentVisitor;
+import com.behavior.sdk.trigger.segment.service.SegmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/segments")
+@RequiredArgsConstructor
+@Tag(name = "Segment", description = "세그먼트 관리 API")
+public class SegmentController {
+
+    private final SegmentService segmentService;
+
+    @PostMapping
+    @Operation(summary = "세그먼트 생성", description = "세그먼트를 생성합니다.")
+    @ApiResponse(responseCode = "201", description = "세그먼트 생성 성공")
+    public ResponseEntity<SegmentResponse> createSegment(@RequestBody SegmentCreateRequest createRequest) {
+
+        Segment segment = segmentService.createSegment(createRequest);
+
+        List<UUID> visitorIds = segment.getSegmentVisitors().stream()
+                .map(SegmentVisitor::getVisitorId)
+                .collect(Collectors.toList());
+
+        SegmentResponse segmentResponse = new SegmentResponse(
+                segment.getId(),
+                segment.getProjectId(),
+                segment.getConditionId(),
+                visitorIds
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(segmentResponse);
+    }
+
+    @GetMapping
+    @Operation(summary = "프로젝트별 모든 세그먼트 조회", description = "프로젝트 ID로 모든 세그먼트를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "세그먼트 목록 조회 성공")
+    public ResponseEntity<List<SegmentResponse>> listResponseEntity(@RequestParam("projectId") UUID projectId) {
+        List<SegmentResponse> segments = segmentService.listSegments(projectId);
+        return ResponseEntity.ok(segments);
+    }
+
+    @GetMapping("/{segmentId}")
+    @Operation(summary = "단일 세그먼트 조회", description = "단일 세그먼트를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "세그먼트 조회 성공")
+    public ResponseEntity<SegmentResponse> getSegment(@PathVariable UUID segmentId) {
+        SegmentResponse segment = segmentService.getSegment(segmentId);
+        return ResponseEntity.ok(segment);
+    }
+
+    @DeleteMapping("/{segmentId}")
+    @Operation(summary = "세그먼트를 soft delete 합니다.", description = "세그먼트를 soft delete 합니다.")
+    @ApiResponse(responseCode = "204", description = "세그먼트 삭제 성공")
+    public ResponseEntity<Void> deleteSegment(@PathVariable UUID segmentId) {
+        segmentService.deleteSegment(segmentId);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/behavior/sdk/trigger/segment/service/SegmentServiceImpl.java
@@ -0,0 +1,91 @@
+package com.behavior.sdk.trigger.segment.service;
+
+import com.behavior.sdk.trigger.condition.entity.Condition;
+import com.behavior.sdk.trigger.condition.repository.ConditionRepository;
+import com.behavior.sdk.trigger.log_event.repository.LogEventRepository;
+import com.behavior.sdk.trigger.segment.dto.SegmentCreateRequest;
+import com.behavior.sdk.trigger.segment.dto.SegmentResponse;
+import com.behavior.sdk.trigger.segment.entity.Segment;
+import com.behavior.sdk.trigger.segment.repository.SegmentRepository;
+import com.behavior.sdk.trigger.segment.repository.SegmentVisitorRepository;
+import com.behavior.sdk.trigger.visitor.repository.VisitorRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SegmentServiceImpl implements SegmentService{
+
+    private final SegmentRepository segmentRepository;
+    private final SegmentVisitorRepository segmentVisitorRepository;
+    private final LogEventRepository logEventRepository;
+    private final VisitorRepository visitorRepository;
+    private final ConditionRepository conditionRepository;
+
+    @Override
+    public Segment createSegment(SegmentCreateRequest request) {
+
+        Condition condition = conditionRepository.findById(request.getConditionId())
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 조건입니다."));
+
+        Integer threshold = condition.getThreshold();
+        String pageUrl = condition.getPageUrl();
+
+        List<UUID> visitorIds = logEventRepository.findVisitorIdsByCondition(condition.getId(), threshold, pageUrl);
+
+        Segment segment = new Segment();
+        segment.setProjectId(request.getProjectId());
+        segment.setConditionId(request.getConditionId());
+        segment.addVisitorsByIds(visitorIds);
+
+        return segmentRepository.save(segment);
+    }
+
+    @Override
+    public List<SegmentResponse> listSegments(UUID projectId) {
+        List<Segment> segments = segmentRepository.findByProjectIdAndDeletedAtIsNull(projectId);
+        return segments.stream()
+                .map(s -> {
+                   List<UUID> visitorIds = s.getSegmentVisitors().stream()
+                           .map(segmentVisitor -> segmentVisitor.getVisitorId())
+                           .toList();
+                   return new SegmentResponse(
+                           s.getId(),
+                           s.getProjectId(),
+                           s.getConditionId(),
+                           visitorIds
+                   );
+                })
+                .toList();
+    }
+
+    @Override
+    public SegmentResponse getSegment(UUID segmentId) {
+        Segment segment = segmentRepository.findById(segmentId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 세그먼트입니다."));
+        List<UUID> visitorIds = segment.getSegmentVisitors().stream()
+                .map(segmentVisitor -> segmentVisitor.getVisitorId())
+                .toList();
+        return new SegmentResponse(
+                segment.getId(),
+                segment.getProjectId(),
+                segment.getConditionId(),
+                visitorIds
+        );
+    }
+
+    @Override
+    public void deleteSegment(UUID segmentId) {
+        Segment segment = segmentRepository.findById(segmentId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 세그먼트입니다."));
+        segment.softDelete();
+        segmentRepository.save(segment);
+    }
+}
