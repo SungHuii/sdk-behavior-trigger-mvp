8183b26fc4965b74e187210212ade1f43cdd8a3b
홍성휘 <gkemg2017@gmail.com>
2025-05-22 19:40:53 +0900

    feat: add list by segmentid repository

--- src/main/java/com/behavior/sdk/trigger/segment/repository/SegmentRepository.java
+++ src/main/java/com/behavior/sdk/trigger/segment/repository/SegmentRepository.java
@@ -2,10 +2,15 @@ package com.behavior.sdk.trigger.segment.repository;
 
 import com.behavior.sdk.trigger.segment.entity.Segment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SegmentRepository extends JpaRepository<Segment, UUID> {
     List<Segment> findByProjectIdAndDeletedAtIsNull(UUID projectId);
+
+    @Query("select v.visitorId from SegmentVisitor v where v.segment.id = :segmentId")
+    List<UUID> findVisitorIdsBySegmentId(@Param("segmentId") UUID segmentId);
 }
--- src/main/java/com/behavior/sdk/trigger/segment/repository/SegmentVisitorRepository.java
+++ src/main/java/com/behavior/sdk/trigger/segment/repository/SegmentVisitorRepository.java
@@ -3,10 +3,15 @@ package com.behavior.sdk.trigger.segment.repository;
 import com.behavior.sdk.trigger.segment.entity.SegmentVisitor;
 import com.behavior.sdk.trigger.segment.entity.SegmentVisitorKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SegmentVisitorRepository extends JpaRepository<SegmentVisitor, SegmentVisitorKey> {
     List<SegmentVisitor> findByIdSegmentId(UUID segmentId);
+
+    @Query("select sv.segmentVisitorKey.visitorId from SegmentVisitor sv where sv.segmentVisitorKey.segmentId = :segmentId")
+    List<UUID> findVisitorIdsBySegmentId(@Param("segmentId") UUID segmentId);
 }
