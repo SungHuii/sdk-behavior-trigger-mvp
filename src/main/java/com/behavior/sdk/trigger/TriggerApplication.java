4473f440bfb975fb1ced66719c44e3c693474748
홍성휘 <gkemg2017@gmail.com>
2025-06-08 15:37:08 +0900

    feat: enable scheduling

--- src/main/java/com/behavior/sdk/trigger/TriggerApplication.java
+++ src/main/java/com/behavior/sdk/trigger/TriggerApplication.java
@@ -2,8 +2,10 @@ package com.behavior.sdk.trigger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TriggerApplication {
 
 	public static void main(String[] args) {
