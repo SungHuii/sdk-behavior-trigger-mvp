48b31a8d7277c93625c8f1b066c3154e51908332
홍성휘 <gkemg2017@gmail.com>
2025-06-17 20:04:29 +0900

    fix: remove entitymanager method savelog

--- src/main/java/com/behavior/sdk/trigger/email_log/repository/EmailLogRepositoryImpl.java
+++ src/main/java/com/behavior/sdk/trigger/email_log/repository/EmailLogRepositoryImpl.java
@@ -5,42 +5,18 @@ import com.behavior.sdk.trigger.email_log.entity.EmailLog;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Repository
+@RequiredArgsConstructor
 public class EmailLogRepositoryImpl implements EmailLogRepositoryCustom {
-
     @PersistenceContext
     private EntityManager em;
 
-    @Override
-    @Transactional
-    public void saveSentLog(UUID visitorId, UUID batchId) {
-
-        EmailLog emailLog = EmailLog.builder()
-                .id(UUID.randomUUID())
-                .visitorId(visitorId)
-                .batchId(batchId)
-                .status(EmailStatus.SENT)
-                .createdAt(LocalDateTime.now())
-                .build();
-        em.persist(emailLog);
-    }
-
-    @Override
-    @Transactional
-    public void saveFailedLog(UUID visitorId, UUID batchId, String message) {
-        EmailLog emailLog = EmailLog.builder()
-                .id(UUID.randomUUID())
-                .visitorId(visitorId)
-                .batchId(batchId)
-                .status(EmailStatus.FAILED)
-                .createdAt(LocalDateTime.now())
-                .errorMessage(message)
-                .build();
-        em.persist(emailLog);
-    }
 
     @Override
     public long countByBatchIdAndStatus(UUID batchId, EmailStatus status) {
