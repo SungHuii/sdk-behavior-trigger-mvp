2a036c1bf0fa1634fdba3a22ace26606beed4084
홍성휘 <gkemg2017@gmail.com>
2025-07-02 20:24:39 +0900

    fix: Cors Error fix

---
+++ src/main/java/com/behavior/sdk/trigger/config/WebConfig.java
@@ -0,0 +1,48 @@
+package com.behavior.sdk.trigger.config;
+
+import com.behavior.sdk.trigger.common.interceptor.ProjectDomainValidationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment env;
+
+    private final ProjectDomainValidationInterceptor interceptor;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/api/logs"); // 로그 관련 API에만 적용
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://example.com")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        } else {
+            registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:3000", "file://")
+                    .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
+                    .allowCredentials(true);
+        }
+    }
+}
