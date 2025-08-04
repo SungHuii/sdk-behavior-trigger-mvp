dedab49bf559a8e20b5adf06b567bbfe1e7086e2
홍성휘 <gkemg2017@gmail.com>
2025-08-04 15:09:21 +0900

    feat: impl HomeController (redirect page)

---
+++ src/main/java/com/behavior/sdk/trigger/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.behavior.sdk.trigger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/login.html";
+    }
+}
